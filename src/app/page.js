f46ca86809641df332d17276b9f15e5bcfced6e4
Vo Minh Khanh <khanhvm@tcom.vn>
2024-05-04 11:36:59 +0700

    animation

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,12 @@ import ProjectSection from "./components/ProjectSection";
 import Contact from "./components/Contact";
 import Experience from './components/Experience';
 import { ScrollTop } from './components/ScrollTop';
+import Snow from "./components/Snow";
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col bg-[#121212]">
+      <Snow />
       <Navbar />
       <div className="container mt-24 mx-auto px-6 lg:px-12: py-4">
         <HeroSection />
