535f99b9f6c6c545aa819da60a827a855ec3be41
Vo Minh Khanh <khanhvm@tcom.vn>
2024-05-04 14:27:11 +0700

    update
    update metadata

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,25 @@ import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
 import Contact from "./components/Contact";
-import Experience from './components/Experience';
-import { ScrollTop } from './components/ScrollTop';
+import Experience from "./components/Experience";
+import { ScrollTop } from "./components/ScrollTop";
 import Snow from "./components/Snow";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col bg-[#121212]">
+    <>
       <Snow />
-      <Navbar />
-      <div className="container mt-24 mx-auto px-6 lg:px-12: py-4">
-        <HeroSection />
-        <AboutSection />
-        <Experience />
-        <ProjectSection />
-        <Contact />
-      </div>
-      <ScrollTop />
-    </main>
+      <main className="relative flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
+        <div className="container mt-24 mx-auto px-6 lg:px-12: py-4">
+          <HeroSection />
+          <AboutSection />
+          <Experience />
+          <ProjectSection />
+          <Contact />
+        </div>
+        <ScrollTop />
+      </main>
+    </>
   );
 }
